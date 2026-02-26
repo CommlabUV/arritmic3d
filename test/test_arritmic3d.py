1d2597244dc56187b1a0333eaec61a512b1c9737
Ignacio (amaltea) <ignacio@amaltea.uv.es>
2026-02-26 07:29:28 +0100

    Completed example script to test the module

--- test/test_arritmic3d.py
+++ test/test_arritmic3d.py
@@ -6,34 +6,47 @@ from IPython.display import clear_output
 import time
 import arritmic3d as a3d
 
-def plot_grid(grid,field="AP",plt_show=False) :
+ranges = {
+    "AP": [-80, 40],
+    "State": [0, 2]
+}
+
+def plot_grid(grid,field="AP",plt_show=False,title = "") :
+    rng = ranges.get(field,None)
+    # Apply a threshold to the grid. Cells with restitution_model==0
+    # are not part of the simulation domain
+    grid = grid.threshold(0.5, scalars="restitution_model", all_scalars=True)
     # Setup plotter for a static image
     plotter = pv.Plotter(off_screen=True)
-    plotter.add_mesh(grid, scalars=field, cmap="coolwarm", show_edges=True)
+    plotter.add_mesh(grid, scalars=field, cmap="coolwarm", show_edges=True,rng=rng)
     plotter.view_xy()
     img = plotter.show(screenshot=True)
     # Display using matplotlib
     plt.imshow(img)
     plt.axis('off')
+    if title != "":
+        plt.title(title)
     if plt_show:
         plt.show()
     return img
 
-def plot_vtk(file_path, field="AP",plt_show=False):
+def plot_vtk(file_path, field="AP",plt_show=False,title = ""):
     grid = pv.read(file_path)
-    grid = grid.threshold(0.5, scalars="restitution_model", all_scalars=True)
-    plot_grid(grid,field=field,plt_show=plt_show)
+    plot_grid(grid,field=field,plt_show=plt_show,title=title)
 
-def plot_animation(case_dir, field="AP"):
+def plot_animation(case_dir, field="AP", init_time=None, end_time=None, step=None):
     config = a3d.load_case_config(case_dir)
-    total_time = config.get("SIMULATION_DURATION")
-    step = config.get("VTK_OUTPUT_PERIOD")
+    if init_time is None:
+        init_time = config.get("VTK_OUTPUT_PERIOD")
+    if step is None:
+        step = config.get("VTK_OUTPUT_PERIOD")
+    if end_time is None:
+        end_time = config.get("SIMULATION_DURATION")
 
-    for time_ms in range(step, total_time + 1, step):
+    for time_ms in range(init_time, end_time + 1, step):
         file_path = f"{case_dir}/slab_{time_ms:05d}.vtk"
         if os.path.exists(file_path):
-            clear_output(wait=True)
-            plot_vtk(file_path, field=field, plt_show=True)
+            plot_vtk(file_path, field=field, plt_show=True, title=f"Time: {time_ms} ms")
             # Small pause to allow the UI to update
             time.sleep(0.01)
 
@@ -62,16 +75,7 @@ print("Simulation completed!")
 
 # --- STEP 3: Visualize a single result ---
 print("\n--- Visualizing a frame at 715ms ---")
-
-# Load the grid (later, we will use a function to do this)
-grid = pv.read("out_test/slab_00715.vtk")
-
-# Filter out regions where restitution_model is 0
-# all_scalars=True ensures only cells where ALL points are > 0.5 are kept
-grid = grid.threshold(0.5, scalars="restitution_model", all_scalars=True)
-
-# Plot the action potential
-plot_grid(grid,"AP",plt_show=True)
+plot_vtk("out_test/slab_00715.vtk",plt_show=True, title="t=715ms")
 
 # --- STEP 4: Re-run Case ---
 # Arritmic3D is designed so that you can easily re-run a simulation
@@ -91,7 +95,7 @@ clean_case_dir(case_dir)
 a3d.arritmic3d(case_dir,config=config)
 print("Simulation finished.")
 
-plot_vtk(case_dir+"/slab_00720.vtk",plt_show=True)
+plot_vtk(case_dir+"/slab_00720.vtk",plt_show=True, title="t=720ms")
 
 # --- STEP 6: Show an animation of the simulation
 print("\n--- Showing an animation of the simulation ---")
