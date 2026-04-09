c8c4b02df0706cccb3b637d9f5957524f8b7f353
Ignacio (amaltea) <ignacio@amaltea.uv.es>
2026-04-09 11:27:32 +0200

    Modify test configuration to get closer to reentry
    Remove old unused code

--- test/test_reentry_disc.py
+++ test/test_reentry_disc.py
@@ -1,6 +1,20 @@
 import os
 import arritmic3d as a3d
 from plot_functions import (plot_vtk, plot_animation, delete_case_dir)
+"""
+Here are the *main characteristics of the domain*:
+- Size: 215 x 215 x 1 elements, with an edge length of 0.35 mm (hexahedral mesh → final dimensions: 75.25 x 75.25 x 0.35 mm)
+- Number of cells: 46,225
+- Number of points: 93,312
+- Mesh type: isotropic
+- The geometry corresponds to a Healthy x ENDO x TorORd model, except for a central region with a radius of 2.5 cm (which, in this case is a HCM Moderate PoM 17 x ENDO x TorORd model. The PoM 17 refers to a set of simulation that I'm doing for a parallel project, don't focus on it for the moment).
+
+*Stimulation protocol:*
+- I apply 3 x S1 stimuli (cycle length = 1000 ms) from the bottom line (see attached screenshot)
+- The S2 stimulus is applied from a point as shown in the attached figure. In my case, this corresponds to point ID 46265
+- With S2 at 377 ms, you should observe a re-entry
+- With S2 at 382 ms, you should observe ventricular tachycardia (i.e., more than one re-entry)
+"""
 
 # We also need the argument parser for the build_slab utility
 import arritmic3d.arr3D_build_slab as arr_build
@@ -23,8 +37,8 @@ slab_args = [
     "--nnodes", "215", "215", "2",
     "--spacing", "0.35", "0.35", "0.35",
     "--region-by-side", "south", "1",
-    "--field", "restitution_model", "2",
-    "--region", '{"shape" : "circle", "cx" : 37.65, "cy" : 37.65, "r1" : 25.0, "r2" : 25.0, "restitution_model" : 5}',
+    "--field", "restitution_model", "1",
+    "--region", '{"shape" : "circle", "cx" : 37.65, "cy" : 37.65, "r1" : 25.0, "r2" : 25.0, "restitution_model" : 2}',
     "--region", '{"shape" : "circle", "cx" : 37.65, "cy" : 4.0,  "r1" : 0.4,   "r2" : 0.4, "activation_region" : 2}'
 ]
 
@@ -39,22 +53,28 @@ plot_vtk(slab_path, field="restitution_model", plt_show=True, title="Slab")
 print("\n--- Configuring simulation ---")
 config = {
     "VTK_INPUT_FILE": slab_path,
-    "APD_MODEL": "TenTuscher",
-    "CV_MODEL": "TenTuscher",
-    "SIMULATION_DURATION": 5500,
+    "APD_MODEL_CONFIG_PATH": "restitution/configDisc_APD.csv",
+    "CV_MODEL_CONFIG_PATH": "restitution/configDisc_CV.csv",
+    "CORRECTION_FACTOR_CV": 0.95,
+    "CORRECTION_FACTOR_APD": 1.1,
+    "INITIAL_APD": 430,
+    "SIMULATION_DURATION": 3500,
     "VTK_OUTPUT_PERIOD": 10,
     "PROTOCOL": [
         {
             "ACTIVATION_REGION": 1,
-            "FIRST_ACTIVATION_TIME": 0,
-            "N_STIMS_PACING": [5],
-            "BCL": [600]
+            "N_STIMS_PACING": [3],
+            "BCL": [1000]
         }
     ],
     "ACTIVATE_NODES": [
         {
             "ACTIVATION_REGION": 2,
-            "ACTIVATION_TIMES": [[2800,10]]
+            "ACTIVATION_TIMES": [[2389,4]]
+        },
+        {
+            "ACTIVATION_REGION": 2,
+            "ACTIVATION_TIMES": [[2800,5]]
         }
     ]
 }
@@ -64,69 +84,6 @@ print("\n--- Running simulation ---")
 a3d.arritmic3d(case_dir, config=config)
 print("Simulation completed!")
 
-exit(0)
-
 # --- STEP 5: Visualize a single result ---
-print("\n--- Visualizing a frame at 500ms ---")
-plot_vtk(os.path.join(case_dir, f"slab_00040.vtk"), plt_show=True, title="t=40ms")
-
-# --- STEP 6: Show an animation of the simulation ---
-print("\n--- Showing an animation of the simulation ---")
-#plot_animation(case_dir, field="AP")
-
-# Clean the case dir
-delete_case_dir(case_dir)
-
-# Ensure the subfolder for the slab exists
-os.makedirs(os.path.join(case_dir, "input_data"), exist_ok=True)
-
-# --- STEP 7: Set a slab with CORE region (model = 7) ---
-print("\n--- Building the slab with CORE region ---")
-slab_args = [
-    slab_path,
-    "--nnodes", "45", "45", "5",
-    "--spacing", "0.1", "0.1", "0.1",
-    "--region-by-side", "south", "1",
-    "--field", "restitution_model", "2",
-    "--region", '{"shape" : "square", "cx" : 2.25, "cy" : 2.25, "r1" : 1.0, "r2" : 1.0, "restitution_model" : 5}',
-    "--region", '{"shape" : "square", "cx" : 2.25, "cy" : 2.25, "r1" : 0.5, "r2" : 0.5, "restitution_model" : 7}'
-]
-
-# Build and save the slab
-a3d.build_slab(args_list=slab_args, save=True)
-print("Slab generated successfully!")
-
-# Visualize the slab
-plot_vtk(slab_path, field="restitution_model", plt_show=True, title="Slab")
-
-# --- STEP 8: Configure the simulation ---
-print("\n--- Configuring simulation ---")
-config = {
-    "VTK_INPUT_FILE": slab_path,
-    "APD_MODEL": "TenTuscher",
-    "CV_MODEL": "TenTuscher",
-    "SIMULATION_DURATION": 1000,
-    "VTK_OUTPUT_PERIOD": 10,
-    "PROTOCOL": [
-        {
-            "ACTIVATION_REGION": 1,
-            "FIRST_ACTIVATION_TIME": 0,
-            "N_STIMS_PACING": [3],
-            "BCL": [400]
-        }
-    ]
-}
-
-# --- STEP 9: Run the simulation ---
-print("\n--- Running simulation ---")
-a3d.arritmic3d(case_dir, config=config)
-print("Simulation completed!")
-
-# --- STEP 10: Visualize the results ---
-print("\n--- Visualizing a frame at 500ms ---")
-plot_vtk(os.path.join(case_dir, f"slab_00040.vtk"), plt_show=True, title="t=40ms")
-plot_vtk(os.path.join(case_dir, f"slab_00060.vtk"), plt_show=True, title="t=60ms")
-
-# Show an animation of the simulation
-print("\n--- Showing an animation of the simulation ---")
-plot_animation(case_dir, field="AP")
+print("\n--- Visualizing a frame at 2550ms ---")
+plot_vtk(os.path.join(case_dir, f"slab_02550.vtk"), plt_show=True, title="t=2550ms")
